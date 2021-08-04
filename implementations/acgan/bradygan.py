a4d0417f85fc11f8d628e6a271553fcbab11c40d
Brady <bradyakruse@gmail.com>
2021-08-04 15:45:39 -0500

    alright this bitch be workin now

--- implementations/acgan/bradygan.py
+++ implementations/acgan/bradygan.py
@@ -34,7 +34,7 @@ print(opt)
 
 cuda = True if torch.cuda.is_available() else False
 
-class Feeder(torch.utils.data.Dataset): #Borrowed from HCN-PyTorch
+class Feeder(torch.utils.data.Dataset):
     """ Feeder for skeleton-based action recognition
     Arguments:
         data_path: the path to '.npy' data, the shape of data should be (N, C, T, V, M)
@@ -50,39 +50,12 @@ class Feeder(torch.utils.data.Dataset): #Borrowed from HCN-PyTorch
                  data_path,
                  label_path,
                  num_frame_path,
-                 random_valid_choose=False,
-                 random_choose=False,
-                 random_shift=False,
-                 random_move=False,
-                 window_size=-1,
-                 normalization=False,
-                 debug=False,
-                 origin_transfer=False,
-                 p_interval=1,
-                 crop_resize=False,
-                 rand_rotate=0,
-                 mmap=False,
                  ):
-        self.debug = debug
         self.data_path = data_path
         self.label_path = label_path
         self.num_frame_path = num_frame_path
-        self.random_choose = random_choose
-        self.random_valid_choose = random_valid_choose
-        self.random_shift = random_shift
-        self.random_move = random_move
-        self.window_size = window_size
-        self.normalization = normalization
-        self.origin_transfer = origin_transfer
-        self.p_interval = p_interval
-        self.crop_resize = crop_resize
-        self.rand_rotate = rand_rotate
-        self.mmap = mmap
-
+    
         self.load_data()
-        self.coordinate_transfer()
-        if normalization:
-            self.get_mean_map()
 
     def load_data(self):
         # data: N C V T M
@@ -105,58 +78,11 @@ class Feeder(torch.utils.data.Dataset): #Borrowed from HCN-PyTorch
             raise ValueError()
 
         # load data
-        if self.mmap == True:
-            self.data = np.load(self.data_path,mmap_mode='r')
-        else:
-            self.data = np.load(self.data_path,mmap_mode=None) # directly load all data in memory, it more efficient but memory resource consuming for big file
+        self.data = np.load(self.data_path,mmap_mode=None) # directly load all data in memory, it more efficient but memory resource consuming for big file
 
         # load num of valid frame length
         self.valid_frame_num = np.load(self.num_frame_path)
 
-        if self.debug:
-            self.label = self.label[0:100]
-            self.data = self.data[0:100]
-            self.num_frame_path = self.num_frame_path[0:100]
-            self.sample_name = self.sample_name[0:100]
-
-        self.N, self.C, self.T, self.V, self.M = self.data.shape
-
-    def coordinate_transfer(self):
-        data_numpy = self.data
-
-        if self.origin_transfer == 2:
-            #  take joints 2  of each person, as origins of each person
-            origin = data_numpy[:, :, :, 1, :]
-            data_numpy = data_numpy - origin[:, :, :, None, :]
-        elif self.origin_transfer == 0:
-            #  take joints 2  of first person, as origins of each person
-            origin = data_numpy[:, :, :, 1, 0]
-            data_numpy = data_numpy - origin[:, :, :, None, None]
-        elif self.origin_transfer == 1:
-            #  take joints 2  of second person, as origins of each person
-            origin = data_numpy[:, :, :, 1, 1]
-            data_numpy = data_numpy - origin[:, :, :, None, None]
-        else:
-            # print('no origin transfer')
-            pass
-
-        self.data = data_numpy
-
-    def get_mean_map(self):
-        data = self.data
-        N, C, T, V, M = data.shape
-        self.mean_map = data.mean(
-            axis=2, keepdims=True).mean(
-            axis=4, keepdims=True).mean(axis=0)
-        # mean_map 3,1,25,1
-        self.std_map = data.transpose((0, 2, 4, 1, 3)).reshape(
-            (N * T * M, C * V)).std(axis=0).reshape((C, 1, V, 1))
-
-        self.min_map = (data.transpose(0, 2, 3, 4, 1).reshape(N * T * V * M, C)).min(axis=0)
-        self.max_map = (data.transpose(0, 2, 3, 4, 1).reshape(N * T * V * M, C)).max(axis=0)
-        self.mean_mean = (data.transpose(0, 2, 3, 4, 1).reshape(N * T * V * M, C)).mean(axis=0)
-
-        print('min_map', self.min_map, 'max_map', self.max_map, 'mean', self.mean_mean)
 
     def __len__(self):
         return len(self.label)
@@ -168,66 +94,10 @@ class Feeder(torch.utils.data.Dataset): #Borrowed from HCN-PyTorch
         # get data
         # input: C, T, V, M
         data_numpy = self.data[index]
-        # if self.mmap = True, the loaded data_numpy is read-only, and torch.utils.data.DataLoader could load type 'numpy.core.memmap.memmap'
-        if self.mmap:
-            data_numpy = np.array(data_numpy) # convert numpy.core.memmap.memmap to numpy
-
         label = self.label[index]
         valid_frame_num = self.valid_frame_num[index]
 
-        # normalization
-        if self.normalization:
-            # data_numpy = (data_numpy - self.mean_map) / self.std_map
-            # be careful the value is for NTU_RGB-D, for other dataset, please replace with value from function 'get_mean_map'
-            # probably need to fix this at some point lol
-            if self.origin_transfer == 0:
-                min_map, max_map = np.array([-4.9881773, -2.939787, -4.728529]), np.array(
-                    [5.826573, 2.391671, 4.824233])
-            elif self.origin_transfer == 1:
-                min_map, max_map = np.array([-5.836631, -2.793758, -4.574943]), np.array([5.2021008, 2.362596, 5.1607])
-            elif self.origin_transfer == 2:
-                min_map, max_map = np.array([-2.965678, -1.8587272, -4.574943]), np.array(
-                    [2.908885, 2.0095677, 4.843938])
-            else:
-                min_map, max_map = np.array([-3.602826, -2.716611, 0.]), np.array([3.635367, 1.888282, 5.209939])
-
-            data_numpy = np.floor(255 * (data_numpy - min_map[:, None, None, None]) / \
-                                  (max_map[:, None, None, None] - min_map[:, None, None, None])) / 255
-
-        # processing
-        if self.crop_resize:
-            data_numpy = tools.valid_crop_resize(data_numpy, valid_frame_num, self.p_interval, self.window_size)
-
-        if self.rand_rotate > 0:
-            data_numpy = tools.rand_rotate(data_numpy, self.rand_rotate)
-
-        if self.random_choose:
-            data_numpy = tools.random_choose(data_numpy, self.window_size, auto_pad=True)
-
-
-        if self.random_valid_choose:
-            data_numpy = tools.valid_choose(data_numpy, self.window_size, random_pad = True)
-        elif self.window_size > 0 and (not self.crop_resize) and (not self.random_choose):
-            data_numpy = tools.valid_choose(data_numpy, self.window_size, random_pad=False)
-
-        if self.random_shift:
-            data_numpy = tools.random_shift(data_numpy)
-
-        if self.random_move:
-            data_numpy = tools.random_move(data_numpy)
-
         return data_numpy, label
-
-    def top_k(self, score, top_k):
-        rank = score.argsort()
-        hit_top_k = [l in rank[i, -top_k:] for i, l in enumerate(self.label)]
-        return sum(hit_top_k) * 1.0 / len(hit_top_k)
-
-    def crop(seff, data, tw, th):
-        _, w, h, _ = data.shape
-        x1 = int(round((w - tw) / 2.))
-        y1 = int(round((h - th) / 2.))
-        return data[:, x1:x1 + tw, y1:y1 + th, :]
 
 def weights_init_normal(m): #Are these good values? I have no clue
     classname = m.__class__.__name__
@@ -318,14 +188,14 @@ discriminator.apply(weights_init_normal)
 
 # Configure data loader
 dataloader = torch.utils.data.DataLoader( #Hardcoded feeder cuz I'm feeling lazy
-            dataset=Feeder(data_path="../../data/lidar_mocap/train_data.npy", label_path="../../data/lidar_mocap/train_label.pkl", num_frame_path="../../data/lidar_mocap/train_num_frame.npy"),
+            dataset=Feeder(data_path="lidar_mocap/train_data.npy", label_path="lidar_mocap/train_label.pkl", num_frame_path="lidar_mocap/train_num_frame.npy"),
             batch_size=64,
-            shuffle=True)
+            shuffle=False)
 '''
 train_features, train_labels = next(iter(dataloader))
 print(f"Feature batch shape: {train_features.size()}")
 print(f"Labels batch shape: {train_labels.size()}")
-print(train_features.shape)
+print(train_features[0])
 print("STOP")
 '''
 
